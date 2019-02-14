7899cca2440bd88d7747a291324a718b66d387f4
Siddhesh Pai <siddhesh004@gmail.com>
2019-02-14 09:54:30 +0530

    Second Commit

--- prs/settings.py
+++ prs/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'untitled.urls'
+ROOT_URLCONF = 'prs.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'untitled.wsgi.application'
+WSGI_APPLICATION = 'prs.wsgi.application'
 
 
 # Database
