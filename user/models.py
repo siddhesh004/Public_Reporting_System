79f6389002ad850abb4b7a1e0cb5aa5f0ed0f27f
Siddhesh Pai <siddhesh004@gmail.com>
2019-02-17 10:41:41 +0530

    Custom Users Added

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_public = models.BooleanField(default=False)
+    is_civic = models.BooleanField(default=False)
+
+
+class Public(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+    class Meta:
+        app_label = 'user'
--- user/urls.py
+++ user/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
 
         path('', views.home_view, name='home'),
+
     ]
