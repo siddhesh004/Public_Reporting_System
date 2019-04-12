85dc16f73158fa9a551e21fd7b9676044e6defd9
Siddhesh Pai <siddhesh004@gmail.com>
2019-04-12 14:53:03 +0530

    Fin

--- siddhesh/views/user.py
+++ siddhesh/views/user.py
@@ -5,6 +5,7 @@ from ..forms import IncidenceForm
 from django.http import HttpResponse,Http404
 from django.db import IntegrityError
 from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
 
 class SignUpView(TemplateView):
     template_name = 'registration/signup.html'
@@ -15,6 +16,7 @@ def home(request):
     return render(request, 'home.html')
 
 
+@login_required
 def IncidenceReport(request):
 
         form = IncidenceForm(request.POST or None)
@@ -32,36 +34,42 @@ def signupview(request):
 
     return render(request, 'signup.html')
 
-
+@login_required
 def upvoteview(request, id):
-
+    obj = get_object_or_404(Incidence, pk=id)
     try:
-        obj = get_object_or_404(Incidence, pk=id)
-        temp = obj.upvotes
-        temp = temp + 1
-        obj.upvotes = temp
-        obj.save()
+
 
         vote_obj = Vote(user=request.user, incidence=obj)
         vote_obj.save()
     except IntegrityError as e:
         return render_to_response("error.html")
+
+
+    temp = obj.upvotes
+    temp = temp + 1
+    obj.upvotes = temp
+    obj.save()
 
     return redirect(reported)
 
-
+@login_required
 def downvoteview(request, id):
+    obj = get_object_or_404(Incidence, pk=id)
+
     try:
-        obj = get_object_or_404(Incidence, pk=id)
-        temp = obj.downvotes
-        temp = temp + 1
-        obj.downvotes = temp
-        obj.save()
+
 
         vote_obj = Vote(user=request.user, incidence=obj)
         vote_obj.save()
     except IntegrityError as e:
         return render_to_response("error.html")
+
+
+    temp = obj.downvotes
+    temp = temp + 1
+    obj.downvotes = temp
+    obj.save()
 
     return redirect(reported)
 
@@ -72,6 +80,7 @@ def reported(request):
 
     return render(request,'reports.html',context={'reports':reports})
 
+@login_required
 def civicreports(request):
 
     reports = Incidence.objects.all()
